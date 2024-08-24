24ebc40d8038c243fb16f36eb3ad500993c7e78d
DenHax <whatxpw@gmail.com>
2024-08-24 03:54:46 +0300

    Edit steam config

--- system/modules/programs/steam/default.nix
+++ system/modules/programs/steam/default.nix
@@ -18,7 +18,7 @@ in
   config = mkIf cfg.enable {
     programs.steam = {
       enable = true;
-      gamescopeSession = true;
+      gamescopeSession.enable = true;
     };
     environment.systemPackages = with pkgs; [ mangohud ];
 
