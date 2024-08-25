2b231ee1407dcf7fabfa0aedf1546376bd48a6d0
DenHax <whatxpw@gmail.com>
2024-08-25 05:31:26 +0300

    Conflict solving

--- system/modules/programs/home-manager/default.nix
+++ system/modules/programs/home-manager/default.nix
@@ -19,7 +19,7 @@ in
 
   config = mkIf cfg.enable {
     home-manager = {
-      useGlobalPkgs = true;
+      useGlobalPkgs = false;
       useUserPackages = true;
       extraSpecialArgs = {
         inherit inputs;
