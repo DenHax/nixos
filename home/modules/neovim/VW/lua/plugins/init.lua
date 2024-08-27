09047843c989e7979478a45df69b186cf41f8717
DenHax <whatxpw@gmail.com>
2024-08-27 18:22:19 +0300

    Add zen mode in nvim-writter config

--- home/modules/neovim/VW/lua/plugins/init.lua
+++ home/modules/neovim/VW/lua/plugins/init.lua
@@ -102,4 +102,12 @@ return {
     ft = "markdown",
     opts = {},
   },
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
