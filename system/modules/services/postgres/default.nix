6e148f467b6a40e25e9a1639af83afe0280488f6
DenHax <whatxpw@gmail.com>
2024-08-31 01:51:38 +0300

    Add IP in psql and fix missing pkgs

--- system/modules/services/postgres/default.nix
+++ system/modules/services/postgres/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 
@@ -20,10 +21,15 @@ in
       settings = {
         port = 5432;
       };
-      ensureDatabases = [ "mydatabase" ];
+      ensureDatabases = [ "testdata" ];
+      enableTCPIP = true;
       authentication = pkgs.lib.mkOverride 10 ''
-        #type database  DBuser  auth-method
-        local all       all     trust
+        #...
+        #type database DBuser origin-address auth-method
+        # ipv4
+        host  all      all     127.0.0.1/32   trust
+        # ipv6
+        host all       all     ::1/128        trust
       '';
     };
   };
