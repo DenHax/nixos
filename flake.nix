bee9d2c1ca587228e7b38884e6c8342b82a28246
DenHax <whatxpw@gmail.com>
2024-09-04 22:52:57 +0300

    I HATE HYPRLAND

--- flake.nix
+++ flake.nix
@@ -22,15 +22,14 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # hyprland = {
-    #   url = "github:hyprwm/Hyprland";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+    };
 
-    # xdghypr = {
-    #   url = "github:hyprwm/xdg-desktop-portal-hyprland/v1.3.3";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    xdghypr = {
+      url = "github:hyprwm/xdg-desktop-portal-hyprland/v1.3.3";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     sops-nix.url = "github:Mic92/sops-nix";
 
--- system/modules/services/hyprland/default.nix
+++ system/modules/services/hyprland/default.nix
@@ -26,12 +26,11 @@ in
   config = mkIf cfg.enable {
     programs.hyprland = {
       enable = true;
-      package = pkgsStable.hyprland;
-
-      portalPackage = pkgsStable.xdg-desktop-portal-hyprland;
-      # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-      # portalPackage =
-      #   inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      # package = pkgsStable.hyprland;
+      # portalPackage = pkgsStable.xdg-desktop-portal-hyprland;
+      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+      portalPackage =
+        inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     };
   };
 }
