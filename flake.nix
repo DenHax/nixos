1711a635bb170ff21aaab4d4409b5be1436dce95
DenHax <whatxpw@gmail.com>
2024-09-04 19:03:38 +0300

    Move hosts in hosts.nix

--- flake.nix
+++ flake.nix
@@ -66,64 +66,7 @@
         legacyDarwinArch
       ];
 
-      # hosts = import ./hosts.nix { inherit systems; };
-      hosts = {
-        nixos = {
-          workstation = {
-            # hostname = "workstation";
-            username = "denhax";
-            platform = linuxArch; # ? legacyLinuxArch
-            isWorkstation = true;
-            wm = "qtile";
-            de = "";
-            cpu = "ryzen";
-            gpu = "nvidia";
-            isGame = true;
-            # isRyzen = true;
-            # isNvidia = true;
-          };
-          laptop = {
-            # hostname = "laptop";
-            username = "denhax";
-            platform = linuxArch; # ? legacyLinuxArch
-            isWorkstation = true;
-            wm = "qtile";
-            de = "";
-            cpu = "intel";
-            gpu = "";
-            isGame = false;
-            # isIntel = true;
-          };
-          serve = {
-            # hostname = "server";
-            username = "dh";
-            platform = linuxArch; # ? legacyLinuxArch
-            isWorkstation = true;
-            cpu = "";
-            # isIntel = false;
-            # isRyzen = false;
-          };
-          rasp = {
-            # hostname = "raspberry";
-            username = "denhax";
-            platform = linuxArmArch; # ? legacyLinuxArch
-            isWorkstation = false;
-            cpu = "";
-            # isIntel = false;
-            # isRyzen = false;
-          };
-        };
-        darwin = {
-          macos = {
-            hostname = "macbook";
-            username = "denhax";
-            platform = darwinArch; # ? legacyDarwinArch
-            isWorkstation = true;
-            cpu = "";
-            # isIntel = false;
-          };
-        };
-      };
+      hosts = import ./hosts.nix { inherit systems; };
     in
     flake-parts.lib.mkFlake { inherit inputs; } {
       # inherit systems;
--- hosts.nix
+++ hosts.nix
@@ -1,54 +1,58 @@
 { systems, ... }:
 {
-  workstationDH = {
-    hostname = "workstation";
-    username = "denhax";
-    platform = systems.linuxArch; # ? legacyLinuxArch
-    isWorkstation = true;
-    isRyzen = true;
-    isNvidia = true;
-    wm = "qtile";
-    de = "";
-    cpu = "ryzen";
-    gpu = "nvidia";
-    isGame = true;
+  nixos = {
+    workstation = {
+      # hostname = "workstation";
+      username = "denhax";
+      platform = systems.linuxArch; # ? legacyLinuxArch
+      isWorkstation = true;
+      wm = "qtile";
+      de = "";
+      cpu = "ryzen";
+      gpu = "nvidia";
+      isGame = true;
+      # isRyzen = true;
+      # isNvidia = true;
+    };
+    laptop = {
+      # hostname = "laptop";
+      username = "denhax";
+      platform = systems.linuxArch; # ? legacyLinuxArch
+      isWorkstation = true;
+      wm = "qtile";
+      de = "";
+      cpu = "intel";
+      gpu = "";
+      isGame = false;
+      # isIntel = true;
+    };
+    serve = {
+      # hostname = "server";
+      username = "dh";
+      platform = systems.linuxArch; # ? legacyLinuxArch
+      isWorkstation = true;
+      cpu = "";
+      # isIntel = false;
+      # isRyzen = false;
+    };
+    rasp = {
+      # hostname = "raspberry";
+      username = "denhax";
+      platform = systems.linuxArmArch; # ? legacyLinuxArch
+      isWorkstation = false;
+      cpu = "";
+      # isIntel = false;
+      # isRyzen = false;
+    };
   };
-  laptopDH = {
-    hostname = "laptop";
-    username = "denhax";
-    platform = systems.linuxArch; # ? legacyLinuxArch
-    isWorkstation = true;
-    isIntel = true;
-    wm = "qtile";
-    de = "";
-    cpu = "intel";
-    gpu = "";
-    isGame = false;
-  };
-  serve = {
-    hostname = "server";
-    username = "dh";
-    platform = systems.linuxArch; # ? legacyLinuxArch
-    isWorkstation = true;
-    isIntel = false;
-    isRyzen = false;
-    cpu = "";
-  };
-  raspDH = {
-    hostname = "raspberry";
-    username = "denhax";
-    platform = systems.linuxArmArch; # ? legacyLinuxArch
-    isWorkstation = false;
-    isIntel = false;
-    isRyzen = false;
-    cpu = "";
-  };
-  macXDH = {
-    hostname = "macbook";
-    username = "denhax";
-    platform = systems.darwinArch; # ? legacyDarwinArch
-    isWorkstation = true;
-    isIntel = false;
-    cpu = "";
+  darwin = {
+    macos = {
+      hostname = "macbook";
+      username = "denhax";
+      platform = systems.darwinArch; # ? legacyDarwinArch
+      isWorkstation = true;
+      cpu = "";
+      # isIntel = false;
+    };
   };
 }
