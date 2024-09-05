6c0d1c9c83e5feee116f152f52beb23af3edfb77
DenHax <whatxpw@gmail.com>
2024-09-05 11:09:29 +0300

    Remove river from home cause bolean

--- home/modules/river/default.nix
+++ home/modules/river/default.nix
@@ -1,23 +1,24 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-
-with lib;
-
-let
-  cfg = config.module.river;
-in
-{
-  options.module = {
-    river.enable = mkEnableOption "Enable river";
-  };
-  config = mkIf cfg.enable {
-    wayland.windowManager.river = {
-      enable = true;
-      xwayland = true;
-    };
-  };
-}
+{ }
+# {
+#   config,
+#   lib,
+#   pkgs,
+#   ...
+# }:
+#
+# with lib;
+#
+# let
+#   cfg = config.module.river;
+# in
+# {
+#   options.module = {
+#     river.enable = mkEnableOption "Enable river";
+#   };
+#   config = mkIf cfg.enable {
+#     wayland.windowManager.river = {
+#       enable = true;
+#       xwayland = true;
+#     };
+#   };
+# }
