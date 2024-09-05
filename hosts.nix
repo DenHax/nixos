6c027c54eedd1c019a855ebecd461ab56642b649
DenHax <whatxpw@gmail.com>
2024-09-05 10:30:08 +0300

    fix bolean

--- hosts.nix
+++ hosts.nix
@@ -19,7 +19,7 @@
       username = "denhax";
       platform = systems.linuxArch; # ? legacyLinuxArch
       isWorkstation = true;
-      wm = "river";
+      wm = "qtile";
       de = "";
       cpu = "intel";
       gpu = "";
