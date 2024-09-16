b494e25d02bd2cd597b87986188dbcf6bae16f85
DenHax <whatxpw@gmail.com>
2024-09-16 10:29:33 +0300

    Null return

--- home/modules/neovim/DH/lua/plugins/zen-mode.lua
+++ home/modules/neovim/DH/lua/plugins/zen-mode.lua
@@ -1,9 +1,10 @@
-return {
-	"folke/zen-mode.nvim",
-	enabled = false,
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
-}
+return {}
+-- return {
+-- 	"folke/zen-mode.nvim",
+-- 	enabled = false,
+-- 	opts = {
+-- 		-- your configuration comes here
+-- 		-- or leave it empty to use the default settings
+-- 		-- refer to the configuration section below
+-- 	},
+-- }
