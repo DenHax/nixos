66428efca284aa7e9d4dfebf9d63ba6294f0d99a
DenHax <whatxpw@gmail.com>
2024-09-16 10:30:34 +0300

    Null return

--- home/modules/neovim/DH/lua/plugins/lorem.lua
+++ home/modules/neovim/DH/lua/plugins/lorem.lua
@@ -1,11 +1,12 @@
-return {
-	"derektata/lorem.nvim",
-	enabled = false,
-	config = function()
-		require("lorem").setup({
-			sentenceLength = "medium",
-			comma_chance = 0.2,
-			max_commas_per_sentence = 2,
-		})
-	end,
-}
+return {})
+-- return {
+-- 	"derektata/lorem.nvim",
+-- 	enabled = false,
+-- 	config = function()
+-- 		require("lorem").setup({
+-- 			sentenceLength = "medium",
+-- 			comma_chance = 0.2,
+-- 			max_commas_per_sentence = 2,
+-- 		})
+-- 	end,
+-- }
