3854e3276536995065ab4c7bbb9e6c554e520454
DenHax <whatxpw@gmail.com>
2024-09-16 10:30:22 +0300

    Null return

--- home/modules/neovim/DH/lua/plugins/markdown-preview.lua
+++ home/modules/neovim/DH/lua/plugins/markdown-preview.lua
@@ -1,21 +1,22 @@
-return {
-	"iamcco/markdown-preview.nvim",
-	enabled = false,
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && npm install",
-	-- build = function()
-	-- 	vim.fn["mkdp#util#install"]()
-	-- end,
-	keys = {
-		{
-			"<leader>cp",
-			ft = "markdown",
-			"<cmd>MarkdownPreviewToggle<cr>",
-			desc = "Markdown Preview",
-		},
-	},
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
-	ft = { "markdown" },
-}
+return {}
+-- return {
+-- 	"iamcco/markdown-preview.nvim",
+-- 	enabled = false,
+-- 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+-- 	build = "cd app && npm install",
+-- 	-- build = function()
+-- 	-- 	vim.fn["mkdp#util#install"]()
+-- 	-- end,
+-- 	keys = {
+-- 		{
+-- 			"<leader>cp",
+-- 			ft = "markdown",
+-- 			"<cmd>MarkdownPreviewToggle<cr>",
+-- 			desc = "Markdown Preview",
+-- 		},
+-- 	},
+-- 	init = function()
+-- 		vim.g.mkdp_filetypes = { "markdown" }
+-- 	end,
+-- 	ft = { "markdown" },
+-- }
