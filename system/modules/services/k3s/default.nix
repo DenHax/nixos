53dd077715b249c8edc85849c2014dc6eaa55153
DenHax <whatxpw@gmail.com>
2024-09-23 13:22:09 +0300

    For conflict solve

--- system/modules/services/k3s/default.nix
+++ system/modules/services/k3s/default.nix
@@ -3,14 +3,14 @@
 with lib;
 
 let
-  cfg = config.module.service.k3s;
+  cfg = config.module.services.k3s;
 in
 {
   options.module = {
-    service.k3s.enable = mkEnableOption "Enable k3s";
+    services.k3s.enable = mkEnableOption "Enable k3s";
   };
   config = mkIf cfg.enable {
-    service.k3s = {
+    services.k3s = {
       enable = true;
     };
   };
