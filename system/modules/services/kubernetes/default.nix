d075d0905f7d3d740dd973731a7b427a0e19a71d
DenHax <whatxpw@gmail.com>
2025-01-24 22:17:51 +0300

    Add kube

---
+++ system/modules/services/kubernetes/default.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.module.services.kubernetes;
+  kubeMasterIP = "10.1.1.2";
+  kubeMasterHostname = "api.kube";
+  kubeMasterAPIServerPort = 6443;
+in
+{
+  options.module = {
+    services.kubernetes.enable = mkEnableOption "Enable kubernetes";
+  };
+  config = mkIf cfg.enable {
+    networking.extraHosts = "${kubeMasterIP} ${kubeMasterHostname}";
+
+    environment.systemPackages = with pkgs; [
+      kompose
+      kubernetes
+      # kubectl
+    ];
+    services.kubernetes = {
+      roles = [
+        "master"
+        "node"
+      ];
+      masterAddress = kubeMasterHostname;
+      apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
+      easyCerts = true;
+      apiserver = {
+        securePort = kubeMasterAPIServerPort;
+        advertiseAddress = kubeMasterIP;
+      };
+      addons.dns.enable = true;
+      kubelet.extraOpts = "--fail-swap-on=false";
+    };
+  };
+}
