fc8da737bcffa3f13bb7732e9dd307bd1486600d
DenHax <whatxpw@gmail.com>
2025-01-28 04:56:03 +0300

    Move fprint to services from laptop config

---
+++ system/modules/services/fprint/default.nix
@@ -0,0 +1,45 @@
+{ config, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.module.services.fprint;
+in
+{
+  options.module = {
+    services.fprint.enable = mkEnableOption "Enables fprint";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      fprintd
+    ];
+
+    services.fprintd.enable = true;
+
+    security.pam.services = {
+      login.fprintAuth = true;
+      sudo.fprintAuth = true;
+      # xscreensaver.fprintAuth = true;
+    };
+    # services.open-fprintd.enable = true;
+    # services.python-validity.enable = true;
+
+    # systemd.services.fprintd = {
+    #   wantedBy = [ "multi-user.target" ];
+    #   serviceConfig.Type = "simple";
+    # };
+
+    # services.fprintd = {
+    #   enable = true;
+    #   tod = {
+    #     enable = true;
+    # driver = pkgs.pkgs.libfprint-2-tod1-goodix;
+    # pkgs.libfprint-2-tod1-goodix; # Goodix driver module
+    # pkgs.libfprint-2-tod1-elan # Elan(04f3:0c4b) driver
+    # pkgs.libfprint-2-tod1-vfs0090; # driver for 2016 ThinkPads
+    # pkgs.libfprint-2-tod1-goodix-550a
+    #   };
+    # };
+  };
+}
