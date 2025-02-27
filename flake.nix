467d41ce40d69ea9bbc4fdf1f5a035d87548f524
DenHax <whatxpw@gmail.com>
2025-02-27 16:58:58 +0300

    refactor: update lockfile and flake to 24.11 and move from unstable to stable (24.11 stable)

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "copyright by DenHax";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     nixos-06cb-009a-fingerprint-sensor = {
