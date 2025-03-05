291e29ca7e3d63a67f752b744f8b3bf0eede94b0
DenHax <whatxpw@gmail.com>
2025-03-05 03:08:50 +0300

    refactor: move module to string with options

--- system/modules/programs/zsh/default.nix
+++ system/modules/programs/zsh/default.nix
@@ -6,8 +6,8 @@ let
   cfg = config.module.programs.zsh;
 in
 {
-  options = {
-    module.programs.zsh.enable = mkEnableOption "Enable zsh";
+  options.module = {
+    programs.zsh.enable = mkEnableOption "Enable zsh";
   };
 
   config = mkIf cfg.enable { programs.zsh.enable = true; };
