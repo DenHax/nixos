36701d766395245c646f1f48ff16e9074c8986ff
DenHax <whatxpw@gmail.com>
2025-03-08 23:21:51 +0300

    config wireguard

--- system/modules/wireguard/default.nix
+++ system/modules/wireguard/default.nix
@@ -1,44 +1,24 @@
-# {
-#   lib,
-#   config,
-#   pkgs,
-#   ...
-# }:
-# with lib;
-#
-# let
-#   cfg = config.module.wireguard;
-# in
-# {
-#   options.module = {
-#     wireguard.enable = mkEnableOption "Enables wireguard";
-#   };
-#
-#   config = mkIf cfg.enable {
-#     networking.firewall = {
-#       allowedUDPPorts = [ 51830 ];
-#     };
-#     networking.wg-quick.interfaces = {
-#       wg0 = {
-#         address = [
-#           "10.0.0.3/32"
-#         ];
-#         dns = [
-#           "10.0.0.1"
-#         ];
-#         privateKeyFile = "/home/denhax/.sec/wir/private";
-#
-#         peers = [
-#           {
-#             publicKey = "H+50yJ72znMwdAPkGvDzz/chJWWunSurrxnreYU8Llo=";
-#             endpoint = "5.39.249.80:51830";
-#             persistentKeepalive = 20;
-#             allowedIPs = [
-#               "0.0.0.0/0"
-#             ];
-#           }
-#         ];
-#       };
-#     };
-#   };
-# }
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.module.wireguard;
+in
+{
+  options.module = {
+    wireguard.enable = mkEnableOption "Enables wireguard";
+  };
+
+  config = mkIf cfg.enable {
+    networking.firewall = {
+      checkReversePath = false;
+      allowedUDPPorts = [ 51830 ];
+    };
+  };
+}
